20b885ab5351a91627cc9694a68828948d15b2c7
Hakan <hknhzl@gmail.com>
2025-01-21 14:04:04 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -177,7 +177,7 @@ if [[ $arch = "arm64" ]]; then
             make_opts="CC=clang"
             host_make_opts="HOSTCC=clang HOSTCXX=clang++"
         else
-            make_opts="CC=clang LD=ld.lld NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy"
+            make_opts="CC=clang LD=ld.lld NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy LLVM_IAS=1 DTC_EXT=dtc"
             make_opts+=" OBJDUMP=llvm-objdump READELF=llvm-readelf LLVM_IAS=1"
             host_make_opts="HOSTCC=clang HOSTCXX=clang++ HOSTLD=ld.lld HOSTAR=llvm-ar"
         fi
